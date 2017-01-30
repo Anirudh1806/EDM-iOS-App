161c8afcdb41227620a2bf016a4a661b1dd85116
admin <admin@admins-MacBook-Pro.local>
2017-01-29 20:28:51 -0600

    Initial Commit created the application

--- EmergencyDisasterManagement/LoginViewController.swift
+++ EmergencyDisasterManagement/LoginViewController.swift
@@ -16,7 +16,7 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //This will adjust the view by sliding the view slghtly upwards
         NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: nil)
         
